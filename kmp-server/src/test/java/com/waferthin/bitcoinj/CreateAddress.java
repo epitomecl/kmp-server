c27c4a0de72c57e03ce5c4436eede0dcf906f3e8
jdlee726 <jdlee726@gmail.com>
2018-04-13 10:17:33 +0900

    apply slf4j logging

--- kmp-server/src/test/java/com/waferthin/bitcoinj/CreateAddress.java
+++ kmp-server/src/test/java/com/waferthin/bitcoinj/CreateAddress.java
@@ -1,26 +1,33 @@
 package com.waferthin.bitcoinj;
 
+import com.epitomecl.kmp.common.HomeConfigurator;
 import org.bitcoinj.core.ECKey;
 import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.core.Address;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
 
 public class CreateAddress {
+    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     public static void main(String[] args) throws Exception {
+        HomeConfigurator.changeLogConfiguration();
 
         // use test net by default
         String net = "test";
 
         if (args.length >= 1 && (args[0].equals("test") || args[0].equals("prod"))) {
             net = args[0];
-            System.out.println("Using " + net + " network.");
+            logger.info("Using " + net + " network.");
         }
 
         // create a new EC Key ...
         ECKey key = new ECKey();
 
         // ... and look at the key pair
-        System.out.println("We created key:\n" + key);
+        logger.info("We created key:\n" + key);
 
         // either test or production net are possible
         final NetworkParameters netParams;
@@ -34,6 +41,6 @@ public class CreateAddress {
         // get valid Bitcoin address from public key
         Address addressFromKey = key.toAddress(netParams);
 
-        System.out.println("On the " + net + " network, we can use this address:\n" + addressFromKey);
+        logger.info("On the " + net + " network, we can use this address:\n" + addressFromKey);
     }
 }
