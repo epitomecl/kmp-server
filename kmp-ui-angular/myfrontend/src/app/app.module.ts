5e58600af70b3cc70d9019136dbec48c6f7a7cf5
jdlee726 <jdlee726@gmail.com>
2018-04-17 13:11:57 +0900

    add features
     @angular/animations
     @angular/cdk
     @angular/material
     hammerjs
    
     @angular/flex-layout

--- kmp-ui-angular/myfrontend/src/app/app.module.ts
+++ kmp-ui-angular/myfrontend/src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+
+import {MymaterialModule} from "./module/mymaterial/mymaterial.module";
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './component/dashboard/dashboard.component';
 
 
-import { AppComponent } from './app.component';
-
+const routes: Routes = [
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(routes)],
+    MymaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
