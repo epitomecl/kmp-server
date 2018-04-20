e3cceaaf68e26445f409838449c763eb4812c66a
jdlee726 <jdlee726@gmail.com>
2018-04-20 16:01:25 +0900

    refactoring

--- kmp-server/src/main/java/com/epitomecl/kmp/cc/main/KmpApp.java
+++ kmp-server/src/main/java/com/epitomecl/kmp/cc/main/KmpApp.java
@@ -2,8 +2,12 @@ package com.epitomecl.kmp.cc.main;
 
 import com.epitomecl.kmp.cc.common.HomeConfigurator;
 import com.epitomecl.kmp.cc.config.SwaggerConfig;
+import com.epitomecl.kmp.dc.entity.hello.Customer;
+import com.epitomecl.kmp.dc.repository.hello.CustomerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,9 +16,13 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.sql.DataSource;
 import java.lang.invoke.MethodHandles;
 import java.net.InetAddress;
+import java.text.SimpleDateFormat;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {
@@ -23,7 +31,7 @@ import java.net.InetAddress;
 @ComponentScan(basePackageClasses = {
         SwaggerConfig.class
 })
-public class KmpApp {
+public class KmpApp implements CommandLineRunner {
     private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     public static void main(String[] args) {
@@ -77,5 +85,40 @@ public class KmpApp {
     public void stop() {
         SpringApplication.exit(ctx);
     }
+
+    //region for test
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public void run(String... args) {
+
+        logger.info("DATASOURCE = " + dataSource);
+
+        logger.info("1.findAll()...");
+        for (Customer customer : customerRepository.findAll()) {
+            logger.info("{}", customer);
+        }
+
+        logger.info("2.findByEmail(String email)...");
+        for (Customer customer : customerRepository.findByEmail("222@yahoo.com")) {
+            logger.info("{}", customer);
+        }
+
+        // For Stream, need @Transactional
+        logger.info("3.findByEmailReturnStream(@Param(\"email\") String email)...");
+        try (Stream<Customer> stream = customerRepository.findByEmailReturnStream("333@yahoo.com")) {
+            stream.forEach(x -> logger.info("{}", x));
+        }
+
+        logger.info("Done!");
+    }
+    //end-region
 
 }
