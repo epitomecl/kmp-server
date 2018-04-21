eb1f5fd02e43b5e174b6dfbab98dbef77cb2294b
jdlee726 <jdlee726@gmail.com>
2018-04-22 05:14:41 +0900

    example fix

--- kmp-99-example-oauth/example-31-authz-server/src/main/java/org/baeldung/config/AuthorizationServerApplication.java
+++ kmp-99-example-oauth/example-31-authz-server/src/main/java/org/baeldung/config/AuthorizationServerApplication.java
@@ -2,10 +2,22 @@ package org.baeldung.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AuthorizationServerApplication extends SpringBootServletInitializer {
+
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory>
+    webServerFactoryCustomizer() {
+        return factory -> {
+            factory.setPort(8081);
+            factory.setContextPath("/spring-security-oauth-server");
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AuthorizationServerApplication.class, args);
