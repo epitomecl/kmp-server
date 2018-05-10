68c512bf689e542aa8b186405ed86aeeb184e8f4
jdlee726 <jdlee726@gmail.com>
2018-05-10 21:14:29 +0900

    ng6 initial commit

--- kmp-35-ui-angular/myfrontend/src/app/app.module.ts
+++ kmp-35-ui-angular/myfrontend/src/app/app.module.ts
@@ -1,29 +1,16 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from "@angular/router";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {MymaterialModule} from "./module/mymaterial/mymaterial.module";
-import {AppComponent} from './app.component';
-import {DashboardComponent} from './component/dashboard/dashboard.component';
-
-
-const routes: Routes = [
-  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-  {path: 'dashboard', component: DashboardComponent},
-];
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DashboardComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    [RouterModule.forRoot(routes)],
-    MymaterialModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
