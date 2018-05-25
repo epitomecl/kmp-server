f9554b84075878cd0fcca7ba2d91e52eae8f0d57
jdlee726 <jdlee726@gmail.com>
2018-05-26 00:00:21 +0900

    remove circular dependencies

--- kmp-35-ui-angular/myfrontend/src/app/home/home.module.ts
+++ kmp-35-ui-angular/myfrontend/src/app/home/home.module.ts
@@ -2,7 +2,8 @@ import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 
 import {MymonoSharedModule} from 'src/app/shared';
-import {HOME_ROUTE, HomeComponent} from './';
+import {HOME_ROUTE} from "./home.route";
+import {HomeComponent} from "./home.component";
 
 @NgModule({
   imports: [MymonoSharedModule, RouterModule.forChild([HOME_ROUTE])],
