d26ee6c870ce769f9174c2e3588502c7ecee2a0b
jdlee726 <jdlee726@gmail.com>
2018-05-26 12:19:24 +0900

    add HTTP_INTERCEPTORS

--- kmp-35-ui-angular/myfrontend/src/app/app.module.ts
+++ kmp-35-ui-angular/myfrontend/src/app/app.module.ts
@@ -1,19 +1,27 @@
 import './vendor.ts';
 
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {Ng2Webstorage} from "ngx-webstorage";
+import {Injector, NgModule} from '@angular/core';
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+
+import {LocalStorageService, Ng2Webstorage, SessionStorageService} from "ngx-webstorage";
+import {JhiEventManager} from "ng-jhipster";
 
 import {MymonoCoreModule} from "./core";
 import {MymonoSharedModule} from "./shared";
-import {ActiveMenuDirective, AppComponent, NavbarComponent} from './layouts';
+import {MyMatModule} from "./my-mat/my-mat.module";
+
+import {AuthInterceptor} from "./blocks/interceptor/auth.interceptor";
+import {AuthExpiredInterceptor} from "./blocks/interceptor/auth-expired.interceptor";
+import {ErrorHandlerInterceptor} from "./blocks/interceptor/errorhandler.interceptor";
+import {NotificationInterceptor} from "./blocks/interceptor/notification.interceptor";
+
+import {MymonoAppRoutingModule} from './app-routing.module';
+import {ActiveMenuDirective, AppComponent, NavbarComponent, PageRibbonComponent} from './layouts';
+import {MymonoHomeModule} from "./home";
 
 import {MyDashboardComponent} from './my-dashboard/my-dashboard.component';
 import {MyTableComponent} from './my-table/my-table.component';
-import {MymonoAppRoutingModule} from './app-routing.module';
-import {PageRibbonComponent} from "./layouts/profiles/page-ribbon.component";
-import {MyMatModule} from "./my-mat/my-mat.module";
-import {MymonoHomeModule} from "./home";
 
 @NgModule({
   imports: [
@@ -34,7 +42,32 @@ import {MymonoHomeModule} from "./home";
     MyDashboardComponent,
     MyTableComponent
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+      deps: [LocalStorageService, SessionStorageService]
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthExpiredInterceptor,
+      multi: true,
+      deps: [Injector]
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorHandlerInterceptor,
+      multi: true,
+      deps: [JhiEventManager]
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: NotificationInterceptor,
+      multi: true,
+      deps: [Injector]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
