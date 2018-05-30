83926958414be42565b9cb061482b02bf8c4f920
jdlee726 <jdlee726@gmail.com>
2018-05-30 23:15:24 +0900

    add prod profile to myfrontend

--- kmp-35-ui-angular/myfrontend/src/app/app.constants.ts
+++ kmp-35-ui-angular/myfrontend/src/app/app.constants.ts
@@ -2,7 +2,11 @@
 // You can add more variables in webpack.common.js or in profile specific webpack.<dev|prod>.js files.
 // If you change the values in the webpack config files, you need to re run webpack to update the application
 
+import {environment} from "../environments/environment";
+
 export const VERSION = 0.1;
 export const DEBUG_INFO_ENABLED: boolean = true;
-export const SERVER_API_URL = location.protocol + '//' + location.hostname + ':8080/';
+export const SERVER_API_URL = (environment.production) ?
+  'https://' + location.hostname + ':8443/' :
+  location.protocol + '//' + location.hostname + ':8080/';
 export const BUILD_TIMESTAMP = 0;
