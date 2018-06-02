803295032a5f9343792506b3b1307b3d39c2f049
jdlee726 <jdlee726@gmail.com>
2018-06-02 10:37:22 +0900

    fix redirection for jenkins-ci #24

--- home/bin/linux/start.sh
+++ home/bin/linux/start.sh
@@ -12,6 +12,6 @@ SERVER_HOME=$( cd "$( dirname "$0" )" && cd .. && cd .. && pwd )
 LOGFILE="start.log"
 CMD_OPTS="--spring.profiles.active=prod"
 
-cmd="nohup java ${JVM_OPTS} -jar "'"'"${UBER_JAR}"'"'" ${CMD_OPTS} 2>&1 | tee ${SERVER_LOG}/${LOGFILE} &"
+cmd="nohup java ${JVM_OPTS} -jar "'"'"${UBER_JAR}"'"'" ${CMD_OPTS} &> ${SERVER_LOG}/${LOGFILE} &"
 echo ${cmd}
 eval ${cmd}
