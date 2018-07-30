0b63ea153f58f3013bd1e9b4ff9d6ea273b31204
jdlee726 <jdlee726@gmail.com>
2018-07-30 12:47:41 +0900

    fix for BlockexplorerApplicationTest

--- kmp-31-blockexplorer/src/main/java/com/epitomecl/kmp/blockexplorer/BlockexplorerApplication.java
+++ kmp-31-blockexplorer/src/main/java/com/epitomecl/kmp/blockexplorer/BlockexplorerApplication.java
@@ -1,13 +1,9 @@
 package com.epitomecl.kmp.blockexplorer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "com.epitomecl.kmp.blockexplorer")
+@SpringBootApplication
 public class BlockexplorerApplication {
 
     public static void main(String[] args) {
