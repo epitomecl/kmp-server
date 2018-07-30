d54c7ef75813a6c489895886028d878966f75331
jdlee726 <jdlee726@gmail.com>
2018-07-30 15:56:14 +0900

    sample TestRestTemplate

--- kmp-31-blockexplorer/src/test/java/com/epitomecl/kmp/blockexplorer/BlockexplorerApplicationTests.java
+++ kmp-31-blockexplorer/src/test/java/com/epitomecl/kmp/blockexplorer/BlockexplorerApplicationTests.java
@@ -6,20 +6,29 @@ import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.lang.invoke.MethodHandles;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class BlockexplorerApplicationTests {
     private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
     public void test_01_wallet_create() {
         logger.info("test_01_wallet_create");
+        String body = this.restTemplate.getForObject("/", String.class);
+        assertThat(body).isEqualTo("Hello World");
     }
 
     @Test
