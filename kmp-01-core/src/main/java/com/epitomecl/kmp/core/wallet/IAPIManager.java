07d108d7e346ca13c4abd8d79a0b57f154f78021
jsyun-epi <jsyun@epitomecl.com>
2018-11-12 16:22:40 +0900

    GitHub vulnerability fix

---
+++ kmp-01-core/src/main/java/com/epitomecl/kmp/core/wallet/IAPIManager.java
@@ -0,0 +1,5 @@
+package com.epitomecl.kmp.core.wallet;
+
+public interface IAPIManager {
+    int spendTXOCount(String address);
+}
--- kmp-01-core/src/main/java/com/epitomecl/kmp/core/wallet/IAPIManager.kt
+++
@@ -1,5 +0,0 @@
-package com.epitomecl.kmp.core.wallet
-
-interface IAPIManager {
-    fun spendTXOCount(address: String) : Int
-}
